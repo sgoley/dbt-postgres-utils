ae322e96fb5ebb652d564364a6db18ceb52086d8
scottmgoley@gmail.com <scottmgoley@gmail.com>
2021-02-19 15:58:14 -0500

    trello ticket: https://trello.com/c/AtYkmUq1/4-update-optimizers-to-be-if-not-exists-create-or-replace

--- macros/optimizers/index.sql
+++ macros/optimizers/index.sql
@@ -1,6 +1,6 @@
 -- original credits to https://github.com/fishtown-analytics/postgres
 {% macro index(this, column) %}
 
-create index if not exists "{{ this.name }}__index_on_{{ column }}" on {{ this }} ("{{ column }}")
+create or replace index if not exists "{{ this.name }}__index_on_{{ column }}" on {{ this }} ("{{ column }}")
 
 {% endmacro %}
