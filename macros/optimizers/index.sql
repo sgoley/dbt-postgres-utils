1c9f7a559a05fb25b22b82d48d5083ac199284b8
scottmgoley@gmail.com <scottmgoley@gmail.com>
2021-02-19 16:16:33 -0500

    set default index type to btree but allow for specification of alternative type

--- macros/optimizers/index.sql
+++ macros/optimizers/index.sql
@@ -1,6 +1,12 @@
 -- original credits to https://github.com/fishtown-analytics/postgres
-{% macro index(this, column) %}
+{% macro index(this, column, indextype=btree) %}
 
-create or replace index if not exists "{{ this.name }}__index_on_{{ column }}" on {{ this }} ("{{ column }}")
+    create or replace index if not exists "{{ this.name }}__index_on_{{ column }}" on {{ this }} 
+    {%- if indextype != 'btree' -%}
+    using {{ indextype }}
+    {%- else -%}
+    using 'btree'
+    {%- endif -%}
+      ("{{ column }}")
 
 {% endmacro %}
